4337d1c8f90fca98f6e523724df2864e02d1d6b7
Plezi Devops <plezidevops@gmail.com>
2022-04-06 17:35:14 +0000

    Using operators to calculate a circle circumference

---
+++ exercises/ex109/main.go
@@ -0,0 +1,13 @@
+// Using operators to calculate a circle circumference
+package main
+
+import "fmt"
+
+func main() {
+	pi := 3.14
+	radius := 5
+	circumference := 2 * pi * float64(radius)
+
+	fmt.Println((circumference))
+	fmt.Printf("For a radius of %v, the circle circumference is %.2f.", radius, circumference)
+}
