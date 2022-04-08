5d31ba37d648e591104c9a1a33fc09fddf88d9f2
Plezi Devops <plezidevops@gmail.com>
2022-04-08 21:08:36 +0000

    removing code no longer needed

--- projects/cards/main.go
+++ projects/cards/main.go
@@ -1,13 +1,8 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print() //call the print function and passed deck reference to it
 
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
