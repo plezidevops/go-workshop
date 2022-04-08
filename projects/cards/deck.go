6d2488eb6cc36d2a1a523d83e0f6d801bc5b7cc1
Plezi Devops <plezidevops@gmail.com>
2022-04-08 21:08:10 +0000

    creating a deck of card

--- projects/cards/deck.go
+++ projects/cards/deck.go
@@ -7,6 +7,21 @@ import "fmt"
 
 type deck []string // here we are defining a new type called deck that extends another type string
 
+func newDeck() deck { // Creating a deck of cards
+	cards := deck{}
+
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // the print function receives a deck (of cards) reference
 func (d deck) print() { // receiver (d deck) on a function
 	for i, card := range d {
