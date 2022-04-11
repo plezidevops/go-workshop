cf46c91e8b36abb8a2fa67cac0366ceb88d1ebd8
Plezi Devops <plezidevops@gmail.com>
2022-04-11 21:37:50 +0000

    if else if else statement

---
+++ the-language/3-control-flow/prog103/main.go
@@ -0,0 +1,17 @@
+// if else if else statement
+
+package main
+
+import "fmt"
+
+func main() {
+	x := "Jeremie"
+
+	if x == "chantal" {
+		fmt.Println("Capital Les Cayes.")
+	} else if x == "jeremie" {
+		fmt.Println("Grand-Anse.")
+	} else {
+		fmt.Println()
+	}
+}
