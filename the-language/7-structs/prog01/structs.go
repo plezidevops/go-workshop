228cbad77ad793d611cd1335541189b0be773edd
Pascal M. Laurent <pascalmlaurent@gmail.com>
2022-04-17 11:15:19 -0400

    using structs

---
+++ the-language/7-structs/prog01/structs.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName string
+	birthdate string
+	createdDate time.Time
+}
+
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+
+	var newUser User
+
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	created := time.Now()
+
+	newUser = User {
+		firstName: firstName,
+		lastName: lastName,
+		birthdate: birthdate,
+		createdDate: created,
+	}
+	// ... do something awesome with that gathered data!
+
+	fmt.Println(newUser)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	userInput, _ := reader.ReadString('\n')
+
+	var cleanedInput string;
+
+	if runtime.GOOS == "windows" {
+		cleanedInput = strings.Replace(userInput, "\r\n", "", -1)
+	} else {
+		cleanedInput = strings.Replace(userInput, "\n", "", -1)
+	}
+	return cleanedInput
+
+}
