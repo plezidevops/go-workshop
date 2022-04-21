2772c51dcc5087e36e5c459efdda2918d4221b81
Pascal M. Laurent <pascalmlaurent@gmail.com>
2022-04-21 05:38:59 -0400

    returning error

--- the-language/3-control-flow/prog107/controlstructures.go
+++ the-language/3-control-flow/prog107/controlstructures.go
@@ -10,13 +10,21 @@ import (
 	"strings"
 )
 
-func main() {
+func getUserAge() (int, error) {
+
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Print("Please enter your age: ")
 	userInput, _ := reader.ReadString('\n')
 	userInput = strings.Replace(userInput, "\n", "", -1)
 	userage, err := strconv.ParseInt(userInput, 0, 64)
+
+	return int(userage), err
+}
+
+func main() {
+
+	userage, err := getUserAge()
 
 	if err != nil {
 		fmt.Println("Invalid input: ", err)
